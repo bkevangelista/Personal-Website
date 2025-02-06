72744f6074d5834842347327224d939096b4182e
Branden Evangelista <evangelistabranden@gmail.com>
2025-02-05 18:22:25 -0700

    feat(backend): create health check api and change route for gcp api

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -3,6 +3,21 @@ from app.utils.gcs_client import list_files
 
 router = APIRouter()
 
-@router.get("/list")
+'''
+Base APIs
+'''
+@router.get("/")
+def landing_api():
+    return {"message": "Welcome to Branden Evangelista's API suite for his personal website!"}
+
+@router.get("/health")
+def health_check():
+    return {"message": "Health check returned successful response!"}
+
+'''
+APIs for GCP Cloud Storage
+'''
+gcpBaseEndpoint = "/external/gcp/cloudStorage"
+@router.get(f"{gcpBaseEndpoint}/list")
 def get_files():
     return list_files()
