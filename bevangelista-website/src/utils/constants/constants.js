e66a6246f1e2063b032858b6ec8ee19620eeb7e1
Branden Evangelista <evangelistabranden@gmail.com>
2025-07-18 20:17:03 -0700

    feature(home): add image from cloud storage on home page and change favicon

---
+++ bevangelista-website/src/utils/constants/constants.js
@@ -0,0 +1,4 @@
+export const IMAGE_CONSTANTS = Object.freeze({
+        MEMOJI_LOGO: "https://storage.googleapis.com/be-website-public/logos/memoji_logo.png",
+        CHICAGO_PIC: "https://storage.googleapis.com/be-website-public/photos/chicago_pic.JPG",
+    });
