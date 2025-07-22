c9a4a602070a9c0d814504376dc3fe550b698b6a
Branden Evangelista <evangelistabranden@gmail.com>
2025-07-21 19:03:19 -0700

    feat(darkMode): initialize dark mode based off system clock

--- bevangelista-website/src/context/DarkModeContext.js
+++ bevangelista-website/src/context/DarkModeContext.js
@@ -1,24 +1,64 @@
 "use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
+import moment from "moment";
+import SunCalc from "suncalc";
 
 const DarkModeContext = createContext();
 
 export const DarkModeProvider = ({ children }) => {
     const [darkMode, setDarkMode] = useState(false);
 
-    // Initial load
-    useEffect(() => {
-        if (
-            localStorage.theme === "dark" ||
-            (!("theme" in localStorage) &&
-                window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ) {
-            setDarkMode(true);
+    const getUserCoordinates = () => {
+        return new Promise((resolve, reject) => {
+            if (!navigator.geolocation) {
+                reject(new Error("Geolocation is not available"));
+                return;
+            }
+
+            navigator.geolocation.getCurrentPosition((position) => {
+                resolve({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            },
+                error => {
+                reject(new Error("Geolocation error: " + error.message));
+            });
+        });
+    }
+
+    const checkIfDarkMode = async () => {
+        try {
+            const { latitude, longitude } = await getUserCoordinates();
+            const now = moment().toDate();
+            const sunTimes = SunCalc.getTimes(now, latitude, longitude);
+
+            return !((now >= sunTimes.sunrise) && (now < sunTimes.sunset));
+        } catch (error) {
+            console.error(error.message);
+            return false;
         }
+    }
+
+    useEffect(() => {
+        async function initDarkMode() {
+            if (
+                localStorage.theme === "dark" ||
+                (!("theme" in localStorage) &&
+                    window.matchMedia("(prefers-color-scheme: dark)").matches)
+            ) {
+                setDarkMode(true);
+            }
+
+            const isDayTime = await checkIfDarkMode();
+            setDarkMode(isDayTime);
+        }
+
+        initDarkMode();
+
     }, []);
 
-    // Sync with localStorage + html class
     useEffect(() => {
         if (darkMode) {
             document.documentElement.classList.add("dark");
@@ -36,5 +76,4 @@ export const DarkModeProvider = ({ children }) => {
     );
 };
 
-// Custom hook for easy use
 export const useDarkMode = () => useContext(DarkModeContext);
