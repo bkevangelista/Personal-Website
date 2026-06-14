1fb79fca8a8c0cf3880f665d95702d1c346bd62f
Branden Evangelista <evangelistabranden@gmail.com>
2026-06-14 09:29:01 -0700

    build(docker): reduce image sizes and fix local build
    build(docker): reduce image sizes

--- bevangelista-website/next.config.mjs
+++ bevangelista-website/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: "standalone",
 	images: {
 		remotePatterns: [
 			{
@@ -8,7 +9,7 @@ const nextConfig = {
 				pathname: "/**",
 			},
 		],
-	},
+	}
 };
 
 export default nextConfig;
